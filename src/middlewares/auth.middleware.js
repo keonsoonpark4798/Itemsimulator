ee4c3e1db1ea8ef375de1ebf55c639939311d7ad
keonsoonpark4798 <98qkrrjstns@gmail.com>
2024-09-12 07:08:56 +0900

    엑세스 토큰 방식 수정

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -5,21 +5,22 @@ import { prisma } from '../utils/prisma/index.js';
 
 export default async function (req, res, next) {
     try {
-        const { authorization } = req.cookies;
+        const authorization = req.headers.authorization;
         if (!authorization) throw new Error('토큰이 존재하지 않습니다.');
 
         const [tokenType, token] = authorization.split(' ');
 
-        if (tokenType !== 'Bearer')
+        if (tokenType !== process.env.TOKEN_TYPE)
             throw new Error('토큰 타입이 일치하지 않습니다.');
-        const decodedToken = jwt.verify(token, 'custom-secret-key');
+
+        const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
+
         const userId = decodedToken.userId;
 
         const user = await prisma.users.findFirst({
             where: { userId },
         });
         if (!user) {
-            res.clearCookie('authorization');
             throw new Error('토큰 사용자가 존재하지 않습니다.');
         }
 
@@ -27,8 +28,6 @@ export default async function (req, res, next) {
         req.user = user;
         next();
     } catch (error) {
-        res.clearCookie('authorization');
-
         // 토큰이 만료되었거나, 조작되었을 때, 에러 메시지를 다르게 출력합니다.
         switch (error.name) {
             case 'TokenExpiredError':
